ddd262651e89ceef6c5b6be5234ff9613ae2e5c3
John Arce <john.arce@gmail.com>
2016-10-20 14:46:57 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# ubuntu_statsfeeder
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: ubuntu_statsfeeder
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
